71ae7cdf69d88b3d45830b170688a87c5e816c5e
VladSelikh <vladikh2405@gmail.com>
2021-01-09 04:01:56 +0300

    feat: complete morse decoder task

--- src/index.js
+++ src/index.js
@@ -38,7 +38,37 @@ const MORSE_TABLE = {
 };
 
 function decode(expr) {
-    // write your solution here
+    let codeArray = [];
+    for (let i = 0; i < expr.length; i++) {
+        if ((i + 1) % 10 === 0) {
+            codeArray.push(expr.substring(i - 9, i + 1));
+        }
+    }
+
+    let dotSlash = codeArray.map(item => {
+        if (item === `**********`) {
+            return ` `;
+        }
+        let numbersToMorse = '';
+        for (let i = 0; i < item.length; i += 2) {
+            if (item[i] !== '0') {
+                numbersToMorse = (item.substring(i, i + 2) === '10')? `${numbersToMorse}.` : `${numbersToMorse}-`;
+            }
+        }
+        return numbersToMorse;
+    })
+    let result = dotSlash.map(item => {
+        if (item === ' ') {
+            return item;
+        }
+
+        for (prop in MORSE_TABLE) {
+            if (prop === item) {
+                return MORSE_TABLE[prop];
+            }
+        }
+    });
+return result.join('');
 }
 
 module.exports = {
